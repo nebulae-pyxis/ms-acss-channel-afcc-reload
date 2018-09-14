e717be19f9f5d47b9cd25f284cf10af50be25574
Felipe Santa <juan.santa@nebulae.com.co>
2018-09-14 16:20:20 -0500

    mongo deployment removed to use the main ACSS mongo deployment

--- backend/acss-channel-afcc-reload/test/crosscutting/dataBase/operations.js
+++ backend/acss-channel-afcc-reload/test/crosscutting/dataBase/operations.js
@@ -40,4 +40,49 @@
 //   error => console.log(error),
 //   () => console.log("Finished")
 // );
+
+
+// Rx.Observable.interval(5000)
+// .mergeMap(() => {
+//   const buNames = [
+//     { name: "Nebula", id: "Nebula" },
+//     { name: "Metro_123", id: "Metro_123" },
+//     { name: "Gana_med", id: "Gana_med" }
+//   ];
+//   return Rx.Observable.of({
+//     amount: Math.floor(Math.random() * 20) * 1000,
+//     timestamp: Date.now(),
+//     bu: buNames[Math.floor(Math.random() * buNames.length)],
+//     afcc: {
+//       before: "card Before",
+//       after: "card After",
+//       UId: "sdosd78gsod8fg6s",
+//       cardId: "234563463546345634",
+//       balanceBefore: 1000,
+//       balanceAfter: 2000
+//     },
+//     source: {
+//       machine: "Nesas-12",
+//       ip: "192.168.1.15"
+//     }
+//   });
+// })
+// .mergeMap(afccEvt =>
+//   eventSourcing.eventStore
+//     .emitEvent$(
+//       new Event({
+//         eventType: "AfccReloadMade",
+//         eventTypeVersion: 1,
+//         aggregateType: "AfccReload",
+//         aggregateId: Date.now(),
+//         data: afccEvt,
+//         user: 'Felipe_santa'
+//       })
+//     )
+// )
+// .subscribe(
+//   ok => { },
+//   error => console.log(error),
+//   () => console.log("Finished")
+// );
     
